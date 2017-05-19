d1844112c5b2eefa9ded41261c450b37be1b24d0
Tasnimul Hasan <thtauhid.99@gmail.com>
2017-05-20 02:43:50 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<form class="" action="index.php" method="post">
+  <fieldset>
+    <legend>Input Your Data Here</legend>
+
+    <input type="text" name="prefix" value="" placeholder="Prefix">
+    <input type="text" name="suffix" value="" placeholder="Suffix"> <br>
+    <input type="text" name="from" value="" placeholder="From">
+    <input type="text" name="to" value="" placeholder="To">
+    <input type="submit" name="" value="Submit">
+  </fieldset>
+</form>
+<form>
+  <fieldset>
+    <legend>Output</legend>
+        <?php
+        $from = $_POST['from'];
+        $to   = $_POST['to'];
+
+        if ($from < $to) {
+          while ($from <= $to) {
+            echo $from . '<br>';
+            $from++;
+          }
+        } elseif ($to < $from) {
+          while ($to <= $from) {
+            echo $from . '<br>';
+            $from--;
+          }
+        }
+        ?>
+</fieldset>
+</form>
